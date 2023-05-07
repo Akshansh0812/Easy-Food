2f6dbaca0474045dc6cdc11a3534c34c2eca940c
Akshansh0812 <akshanshanand2001@gmail.com>
2023-05-08 00:48:50 +0530

    Use retrofit and Glide library

--- app/src/main/java/com/example/easyfood/activities/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.easyfood.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.easyfood.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,8 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.btm_nav)
-        val navController = Navigation.findNavController(this, R.id.host_fragment)
+        val navController = Navigation.findNavController(this,R.id.host_fragment)
 
         NavigationUI.setupWithNavController(bottomNavigation,navController)
 
---
+++ app/src/main/java/com/example/easyfood/retrofit/MealApi.kt
@@ -0,0 +1,10 @@
+package com.example.easyfood.retrofit
+
+import com.example.easyfood.pojo.MealList
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MealApi {
+    @GET("random.php")
+    fun getRandomMeal(): Call<MealList>
+}
